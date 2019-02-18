4dfd63397a583b22a27da76e898b0d82cb0784cb
KMSkelton <kmskelton@outlook.com>
2019-02-17 22:15:13 -0800

    setup to deploy on Netlify

---
+++ hgvf/build/precache-manifest.837d6d34cf71ce65b57f919c79efe5c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee582f7339abba73ddea",
+    "url": "/static/js/main.ee582f73.chunk.js"
+  },
+  {
+    "revision": "14b537072f7598f9dc45",
+    "url": "/static/js/1.14b53707.chunk.js"
+  },
+  {
+    "revision": "ee582f7339abba73ddea",
+    "url": "/static/css/main.1553881a.chunk.css"
+  },
+  {
+    "revision": "bbf7f8f45a7fc291cf983538b451e20a",
+    "url": "/index.html"
+  }
+];
