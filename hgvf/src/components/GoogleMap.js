bbd0c366fbc5ea0f5ccc9fa2a431cb2eb09224a0
KMSkelton <kmskelton@outlook.com>
2019-02-19 00:36:36 -0800

    added Google Map to Home component

---
+++ hgvf/src/components/GoogleMap.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  height: '80rem',
+  width: '80rem',
+};
+const containerStyle={
+  left: '50%',
+  top: '22%'
+}
+export class MapContainer extends Component {
+  render() {
+    return(
+      <Map 
+        style={mapStyles}
+        containerStyle={containerStyle}
+        google={this.props.google}
+        zoom={13}
+        initialCenter={{
+          lat: 22.205079,
+          lng: -159.500566
+        }}
+      />
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY
+})(MapContainer)
